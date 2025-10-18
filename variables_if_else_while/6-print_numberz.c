5c26302e2184d61da5cd20df0739fe8c5c43fada
11646-source <1146@holbertonstudents.com>
2025-10-18 08:23:33 +0000

    nunberz

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * 
+ * Description: Prints all single numbers of base 10 starting from 0
+ * followed by a newline
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	putchar("0123456789"[1]);
+	putchar('\n');
+	return (0);
+}
+
