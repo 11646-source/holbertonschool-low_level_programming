7893711942c0a2745778b800f870da48805c9959
11646-source <1146@holbertonstudents.com>
2025-10-19 06:58:36 +0000

    rev

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ *
+ *
+ *
+ */
+int main(void)
+{
+	int alpharev = 'z';
+
+	while (alpharev >= 'a')
+	{
+		putchar(alpharev);
+		alpharev--;
+	}
+	putchar('\n');
+	return (0);
+}
+	
