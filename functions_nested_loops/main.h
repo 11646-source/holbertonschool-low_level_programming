02295a10fe79ac311831b1f52cfcd04cbf1c51e3
11646-source <1146@holbertonstudents.com>
2025-10-21 07:07:40 +0000

    isalpha

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -2,7 +2,7 @@
 #define MAIN_H
 void print_alphabet_x10(void);
 int _putchar(char c);
-void print_alphat(void);
+void print_alphatbet(void);
  int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
