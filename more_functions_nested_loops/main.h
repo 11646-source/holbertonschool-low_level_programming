c5fbb4340ba3ef87589aa828e2ab2a4945a8c3c9
11646-source <1146@holbertonstudents.com>
2025-10-22 19:59:56 +0000

    issupper

---
+++ more_functions_nested_loops/main.h
@@ -0,0 +1,22 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void _puts(char *str);
+int _putchar(char c);
+void print_rev(char *s);
+void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *strn);
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_diagonal(int n);
+
+#endif /* MAIN_H */
+
