54376ed13d7cf2f0f3837148ad2fd70cb8da8ac6
11646-source <1146@holbertonstudents.com>
2025-11-02 10:28:05 +0000

    sc

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -13,7 +13,7 @@ void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 char *_strcat(char *dest, char *src);
 void _puts_recursion(char *s);
-
+char *_strncat(char *dest, char *src, int n);
 
 
 
