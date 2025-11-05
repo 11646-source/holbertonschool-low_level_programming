b944f1ae8a98fba8d8dfbbcceba8a5bd9ef06e9c
11646-source <1146@holbertonstudents.com>
2025-11-05 21:30:50 +0000

    leet

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * leet converts a specific letters in to a string to "leet" equivalents
+ * @s: Input string
+ * Return: change string
+ */
+char *leet(char *s)
+{
+	char map[256] = {0};
+	char letters[] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
+	char replace[] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'};
+	int i;
+	char *p = s;
+	for (i = 0; i < 10; i++)
+		map[(unsigned char) letters[i]] = replace[i];
+
+	while (*p)
+	{
+		if (map[(unsigned char)*p]) *p = map[(unsigned char)*p];
+		p++;
+	}
+	return (s);
+}
