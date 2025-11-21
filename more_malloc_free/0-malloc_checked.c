4928292964474d5272bffaf1e083cad6dddbc59f
11646-source <1146@holbertonstudents.com>
2025-11-21 10:39:02 +0000

    check malloc

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 /**
- * malloc checked - allocates using memory of malloc
+ * malloc_checked - allocates using memory of malloc
  * @b: The number of bytes to be allocated
  *
  * Return: A pointer to the allocated
