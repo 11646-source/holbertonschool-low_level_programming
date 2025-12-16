ba81b8f47b42b366618aa933c478e7a3cf5bc041
11646-source <11646@holbertonstudents.com>
2025-12-16 06:13:18 +0000

    fln

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -1,14 +1,17 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <stdlib.h>
+
 /**
- * @filename: The name of the file to read
- * @letters: The number of letters to read
+ * @filename: The name of the file to read.
+ * @letters: The number of letters to read.
  */
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
-	char *buffer;
+	char *buf;
+	ssize_t bytes_read;
 	ssize_t bytes_written;
 
 	if (filename == NULL)
@@ -21,22 +24,28 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	{
 		return (0);
 	}
-	buffer = malloc(sizeof(char) * letters);
-	if (buffer == NULL)
+	buf = malloc(sizeof(char) * letters);
+	if (buf == NULL)
 	{
 		close(fd);
 		return (0);
 	}
-	bytes_read = read(fd, buffer, letters);
+	bytes_read = read(fd, buf, letters);
 	if (bytes_read == -1)
 	{
-		free(buffer);
+		free(buf);
 		close(fd);
 		return (0);
 	}
-	free(buffer);
+	bytes_written = write(STDOUT_FILENO, buf, bytes_read);
+	if (bytes_written == -1 || bytes_written != bytes_read)
+	{
+		free(buf);
+		close(fd);
+		return (0);
+	}
+	free(buf);
 	close(fd);
 
 	return (bytes_written);
 }
-
