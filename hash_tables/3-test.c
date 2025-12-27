755b2cdbe60645bd4c4ebeb50d624849cdc2416a
root <11646@holbertonstudents.com>
2025-12-27 16:19:41 +0000

    H.table

---
+++ hash_tables/3-test.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "hash_tables"
+
+int main(void)
+{
+	hash_table_t *ht;
+
+	if (hash_table_s(ht, "hetairas", "hetairas_value_1") == 0)
+		printf("Failed to add\n");
+	else
+		printf("added");
+	return (0);
+}
