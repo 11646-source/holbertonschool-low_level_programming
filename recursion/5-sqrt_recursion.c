5ba4eb320e5cd8cff0ffa5b23409b789b8e83595
root <11646@holbertonstudents.com>
2026-01-03 10:52:34 +0000

    sqrt

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,28 @@
+/**
+ * is_prime_number - Checks if a number is prime
+ * @n: The number to check
+ *
+ * Return: 1 if the number is prime, otherwise 0
+ *
+ * Description: This function checks whether the given integer n is 
+ * a prime number using a recursive approach.
+ */
+int sqrt2(int a, int b)
+{
+	if (b * b == a)
+		return (b);
+	else if (b * b > a)
+		return (-1);
+	return (sqrt2(a, b + 1));
+}
+/**
+ * _sqrt_recursion - returns the natural square root of n
+ * @n: Number Integer
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _sqrt_recursion(int n)
+{
+	return (sqrt2(n, 1));
+}
