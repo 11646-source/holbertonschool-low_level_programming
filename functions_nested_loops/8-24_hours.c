39f0fc4db2ee1adaa9381e3e75dd7432cc7605fc
Ryan364412 <bundhoor50@gmail.com>
2026-08-03 14:54:51 +0400

    update

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -1,37 +1,34 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
- * jack_bauer - Prints every minute of the day of Jack Bauer
+ * jack_bauer - Prints every minute of the day of Jack Bauer.
+ * Description: This function prints the time from 00:00 to 23:59,
+ * one minute at a time, followed by a new line.
  *
- * Description: This function uses nested loops to print
- * every minute of the day in 24-hour format, starting
- * from 00:00 to 23:59. Each time is printed on a new line.
- *
- * Return: Nothing (void)
+ * Return: void (The function does not return a value).
  */
-#include<stdio.h>
-#include "main.h"
-
-void jack_bauer(void)	
+void jack_bauer(void)
 {
 	int hour;
-	int minute;	
+	int minute;
 
 	for (hour = 0; hour <= 23; hour++)
 	{
 		for (minute = 0; minute <= 59; minute++)
 		{
+			/* Print the first digit of the hour */
 			_putchar((hour / 10) + '0');
+			/* Print the second digit of the hour */
 			_putchar((hour % 10) + '0');
+
 			_putchar(':');
+
+			/* Print the first digit of the minute */
 			_putchar((minute / 10) + '0');
+			/* Print the second digit of the minute */
 			_putchar((minute % 10) + '0');
+
 			_putchar('\n');
 		}
-		
 	}
-
-
-
 }
