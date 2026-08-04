f98d41c9a0ba2fe1a8f45b28c3e81f015c55240c
Ryan364412 <bundhoor50@gmail.com>
2026-08-04 09:20:10 +0400

    print.n

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -1,23 +1,33 @@
 #include <stdio.h>
+
 /**
- * main - Entry point
+ * main - Prints all single digit numbers of base 10 from 0.
  *
- * Description: Prints all single numbers of base 10 starting from 0
- * followed by a newline
+ * Description: This program uses a for loop with an integer variable
+ *              to count from 0 to 9. It converts each integer to its
+ *              ASCII character representation by adding the value of '0'
+ *              before printing with putchar. It avoids using any char
+ *              variables and uses putchar exactly twice.
  *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-	int n;
+	int number;
 
-	for (n = 0; n < 10; n++)
+	for (number = 0; number < 10; number++)
 	{
-		putchar('0' + n);
-		
+		/*
+		 * This is the first use of putchar.
+		 * The ASCII value for '0' is 48, '1' is 49, etc.
+		 * By adding the integer `number` to '0', we get the
+		 * correct ASCII code for the digit we want to print.
+		 */
+		putchar(number + '0');
 	}
-	putchar('\n');	
+
+	/* This is the second use of putchar. */
+	putchar('\n');
+
 	return (0);
 }
-
